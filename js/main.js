0c44906988879302a7b21b04a2a59a19ea2d430a
verstka-kunza <99265373+verstka-kunza@users.noreply.github.com>
2022-02-20 19:56:11 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function () {
+
+  $('.open-search').click(function (e) {
+    e.preventDefault();
+    $('#search').addClass('active');
+  });
+
+  $('.close-search').click(function () {
+    $('#search').removeClass('active');
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $('#top').fadeIn();
+    } else {
+      $('#top').fadeOut();
+    }
+  });
+
+  $('#top').click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 700);
+  });
+
+});
